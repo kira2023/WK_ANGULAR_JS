421be3e721d579d3d1dcbc779a66fcb841019dba
Karyna Kutsyia <Karyna_Kutsyia@epam.com>
2019-11-13 11:38:18 +0300

    fix comments about directive

--- app/pages/course-list-page/directives/course-status.js
+++ app/pages/course-list-page/directives/course-status.js
@@ -1,16 +1,21 @@
 angular
     .module('myApp')
     .directive('courseStatus', function() {
-        return function(_, element, attrs) {
-            var courseDate = moment(attrs.courseStatus);
-            var currentDate = moment();
-            var actualCourseDeadline = moment().subtract(15, 'day');
+        return {
+            scope: {
+                courseStatusDate: '='
+            },
+            link: function($scope, element) {
+                var courseDate = moment($scope.courseStatusDate);
+                var currentDate = moment();
+                var actualCourseDeadline = moment().subtract(15, 'day');
 
-            if (courseDate.isBetween(actualCourseDeadline, currentDate, 'day')) {
-                element.addClass('actual-course');
-            }
-            if (courseDate.isSame(currentDate, 'day')) {
-                element.addClass('new-course');
+                if (courseDate.isBetween(actualCourseDeadline, currentDate, 'day')) {
+                    element.addClass('actual-course');
+                }
+                if (courseDate.isSame(currentDate, 'day')) {
+                    element.addClass('new-course');
+                }
             }
         };
     });
